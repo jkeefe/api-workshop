d1c28ba59c000c21e5a71346b78d3484b5844428
John Keefe <john@reallygoodsmarts.nyc>
2022-03-02 23:28:29 -0500

    fix quakes

--- examples/quake_slacker.js
+++ examples/quake_slacker.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-const axios = require("axios");
-
-const usgs_url =
-  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson";
-const SLACK_WEBHOOK = process.env.SLACK_WEBHOOK;
-
-const getQuakes = async () => {
-    const response = await axios.get(usgs_url);
-    const quake = response.data.features[0].properties;
-    return quake;
-};
-
-const sendSlack = async (quake) => {
-    const message = {
-        blocks: [
-            {
-                type: "section",
-                text: {
-                    type: "mrkdwn",
-                    text: `:zap: *Earthquake!*\n\n There was a Magnitude ${quake.mag} earthquake ${quake.place}. <${quake.url}|Here's the USGS link.>`,
-                },
-            },
-        ],
-    };
-
-    axios
-        .post(SLACK_WEBHOOK, message)
-        .then((res) => {
-            console.log(`Slack message sent! Status Code: ${res.status}`);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-};
-
-getQuakes().then(sendSlack);
-=======
 const axios = require('axios');
 
 const usgs_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson"
@@ -75,4 +37,4 @@ const sendSlack = async (quake) => {
 
 getQuakes()
     .then(sendSlack)
->>>>>>> glitch
+
